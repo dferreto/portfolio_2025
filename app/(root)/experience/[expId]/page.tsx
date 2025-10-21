7aeb30af136911ccdcb2c0cb08670a54acda29e0
dferreto <ferretomoraga22@outlook.com>
2025-10-20 18:13:12 -0600

    correction name image

--- app/(root)/experience/[expId]/page.tsx
+++ app/(root)/experience/[expId]/page.tsx
@@ -10,7 +10,8 @@ import CustomTooltip from "@/components/ui/custom-tooltip";
 import { Experiences } from "@/config/experience";
 import { siteConfig } from "@/config/site";
 import { cn, formatDateFromObj } from "@/lib/utils";
-import anaimg from "@/public/ana-img.jpg.jpeg";
+import dinierimg from "@/public/perfil.jpg";
+
 
 interface ExperiencePageProps {
   params: {
@@ -18,7 +19,7 @@ interface ExperiencePageProps {
   };
 }
 
-const githubUsername = "anarodme";
+const githubUsername = "dferreto";
 
 export default function Experience({ params }: ExperiencePageProps) {
   let exp = Experiences.find((val) => val.id === params.expId);
@@ -71,15 +72,15 @@ export default function Experience({ params }: ExperiencePageProps) {
             className="flex items-center space-x-2 text-sm"
           >
             <Image
-              src={anaimg}
-              alt={"ana"}
+              src={dinierimg}
+              alt={"dinier"}
               width={42}
               height={42}
               className="rounded-full bg-background"
             />
 
             <div className="flex-1 text-left leading-tight">
-              <p className="font-medium">{"Ana Rodríguez"}</p>
+              <p className="font-medium">{"Dinier Ferreto"}</p>
               <p className="text-[12px] text-muted-foreground">
                 @{siteConfig.username}
               </p>
